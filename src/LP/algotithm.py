07ee9881aaaf7657799bf63c1a21ba2c3715e37b
Giudelsa <giuditta.delsarto@outlook.it>
2024-05-10 12:36:09 +0200

    inverse filter frequency domain

--- src/LP/algotithm.py
+++ src/LP/algotithm.py
@@ -60,6 +60,29 @@ def adaptive_inverse_filtering(signal, nIterations, predicted_signal, residual_s
     
     return inverse_filter_coeffs
 
+def inverse_filter_frequency_domain(Yr, mu, M):
+    # Initialize G
+    G= np.arange(len(Yr))
+    # Function to calculate FFT of f(t)
+    def fft_f_t(z_square, z_cube, z_fourth):
+        return 4 * (z_square * z_cube - z_fourth * np.sqrt(z_square)) / (z_square**3)
+
+    for n in range(len(Yr)):
+        # Calculate F(m)
+        F_m = fft_f_t(Yr[n] ** 2, Yr[n] ** 3, Yr[n] ** 4)
+
+        # Calculate G'(n+1)
+        G_prime = G + (mu / M) * np.sum(F_m)
+
+        # Normalize G'(n+1)
+        G = G_prime / np.abs(G_prime).max()
+
+    # Convert G from frequency domain to time domain using IFFT
+    g_time_domain = np.fft.ifft(G)
+    g_time_domain = np.abs(g_time_domain)
+
+    return g_time_domain
+
 
 def spectral_subtraction(z, fs, wlen=64e-3, hop=8e-3, nfft=1024, ro_w=7, a_w=5, gS=0.32, epS=1e-3, nu1=0.05, nu2=4):
     # STFT calculation
@@ -88,10 +111,14 @@ def spectral_subtraction(z, fs, wlen=64e-3, hop=8e-3, nfft=1024, ro_w=7, a_w=5, 
     Ez = np.sum(np.abs(Sz)**2, axis=0) / Sz.shape[0]
     Exh = np.sum(np.abs(Sxh)**2, axis=0) / Sxh.shape[0]
 
+
     # Adjust spectral envelope
     P_att = np.ones(Sz.shape[1])
     P_att[(Ez < nu1) & (Ez / Exh > nu2)] = 1e-3
-    Sxh *= np.diag(P_att)
+    # Expand P_att to match the shape of Sxh
+    expanded_P_att = np.tile(P_att, (Sxh.shape[0], 1))
+    # Multiply Sxh with expanded_P_att
+    Sxh *= expanded_P_att
 
     # Inverse STFT to reconstruct signal
     xh = istft(Sxh, fs, nperseg=wlen_samples, noverlap=wlen_samples - hop_samples, nfft=nfft)[1]
@@ -100,10 +127,16 @@ def spectral_subtraction(z, fs, wlen=64e-3, hop=8e-3, nfft=1024, ro_w=7, a_w=5, 
     return xh
 
 
-def LP_dereverberation(signal):
+
+
+
+
+def LP_dereverberation(signal, mu=0.1, nIterations=100):
     # Apply linear prediction analysis to the signal
     predicted_signal, residual_signal = linear_prediction_analysis(signal)
-    coefficients = adaptive_inverse_filtering(signal, 100, predicted_signal, residual_signal)
-    dereverberated_signal = spectral_subtraction(signal, 16000)
+    inverse_filter = adaptive_inverse_filtering(signal, 100, predicted_signal, residual_signal)
+    # inverse_filter = inverse_filter_frequency_domain(residual_signal, mu, 10)
+    inverse_filtered_signal = lfilter(inverse_filter, 1, signal)
+    dereverberated_signal = spectral_subtraction(inverse_filtered_signal, 16000)
 
-    return dereverberated_signal, coefficients
+    return dereverberated_signal, inverse_filter
